b97af3afc693da45a7eab7287e583c54490a9468
andres <andres06energy@hotmail.com>
2022-05-04 21:53:52 -0300

    Updated Annotation Controller

--- src/main/java/com/api/rest/v1/controllers/ProductoController.java
+++ src/main/java/com/api/rest/v1/controllers/ProductoController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 
 @RestController
 @RequestMapping("/v1/productos")
+@CrossOrigin()
 public class ProductoController {
 
 	@Autowired
