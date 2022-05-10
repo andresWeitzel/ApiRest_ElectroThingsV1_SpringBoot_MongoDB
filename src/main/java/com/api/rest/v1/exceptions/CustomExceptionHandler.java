a5f2860b9c0781177926ac47142d1958d30c2cb0
andres <andres06energy@hotmail.com>
2022-05-10 20:02:36 -0300

    Updated Exceptions

--- src/main/java/com/api/rest/v1/exceptions/CustomExceptionHandler.java
+++ src/main/java/com/api/rest/v1/exceptions/CustomExceptionHandler.java
@@ -9,15 +9,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.api.rest.v1.exceptions.producto.ProductoIdMismatchException;
 import com.api.rest.v1.exceptions.producto.ProductoNotFoundException;
 
 @RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler({ProductoNotFoundException.class})
-    protected ResponseEntity<Object> ComponenteHandleNotFoundException(Exception ex, WebRequest request)
+    protected ResponseEntity<Object> ProductoHandleNotFoundException(Exception ex, WebRequest request)
     {
         return handleExceptionInternal(ex, "Producto No Encontrado ",new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+	
+	
+	@ExceptionHandler({ProductoIdMismatchException.class})
+    protected ResponseEntity<Object> ProductoHandleIdMismatchException(Exception ex, WebRequest request)
+    {
+        return handleExceptionInternal(ex, "Producto según el Id No Encontrado ",new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 	
 	
