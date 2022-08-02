03a3321fc96221201e9890ca6d4944fedad17e00
Andrés Weitzel <andres96energy@hotmail.com>
2022-08-02 15:58:49 -0300

    Updated AuthController

--- src/main/java/com/api/rest/v1/security/jwt/JwtProvider.java
+++ src/main/java/com/api/rest/v1/security/jwt/JwtProvider.java
@@ -5,11 +5,14 @@ import com.api.rest.v1.security.entities.UsuarioDetails;
 
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 
@@ -34,21 +37,30 @@ public class JwtProvider {
 	    private int expiration;
 
 	    public String generateToken(Authentication authentication){
+	    	
 	        UsuarioDetails usuarioPrincipal = (UsuarioDetails) authentication.getPrincipal();
-	        return Jwts.builder().setSubject(usuarioPrincipal.getUsername())
+	        
+	        
+	        List<String> roles = usuarioPrincipal.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+	        
+	        
+	        return Jwts
+	        		.builder()
+	        		.setSubject(usuarioPrincipal.getUsername())
+	        		 .claim("roles", roles)
 	                .setIssuedAt(new Date())
 	                .setExpiration(new Date(new Date().getTime() + expiration * 1000))
-	                .signWith(SignatureAlgorithm.HS512, secret)
+	                .signWith(SignatureAlgorithm.HS512, secret.getBytes())
 	                .compact();
 	    }
 
 	    public String getUsernameFromToken(String token){
-	        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+	        return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody().getSubject();
 	    }
 
 	    public boolean validateToken(String token){
 	        try {
-	            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+	            Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token);
 	            return true;
 	        }catch (MalformedJwtException e){
 	            logger.error("token mal formado");
